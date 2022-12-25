2fe4d2f139fbca16f6f42c081f979a91183aeee3
yeonjoo.lee <teddyjune@naver.com>
2022-12-25 16:12:09 +0900

    image picker로 사진선택/FutureBuilder로 사진 표시

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyGalleryApp(),
+    );
+  }
+}
+
+class MyGalleryApp extends StatefulWidget {
+  const MyGalleryApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyGalleryApp> createState() => _MyGalleryAppState();
+}
+
+class _MyGalleryAppState extends State<MyGalleryApp> {
+  //이미지 픽커를 사용한 사진 선택
+  final ImagePicker _picker = ImagePicker();
+  List<XFile>? images;
+
+  @override
+  void initState() {
+    super.initState();
+    loadImages();
+  }
+
+  Future<void> loadImages() async {
+    images = await _picker.pickMultiImage();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('전자액자'),
+      ),
+      body: images == null
+          ? Center(child: Text('No Data'))
+          : FutureBuilder<Uint8List>(
+              future: images![0].readAsBytes(),
+              builder: (context, snapshot) {
+                final data = snapshot.data;
+                if (data == null ||
+                    snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return Image.memory(
+                  data,
+                  width: double.infinity,
+                );
+              }),
+    );
+  }
+}
